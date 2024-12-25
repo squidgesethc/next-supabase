e5bb14cfad4349fb833b81da960b312744ea739e
squidgesethc <squidgeseth@gmail.com>
2024-12-25 15:55:18 +0000

    Added Simple Error handling to login page

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,3 +1,6 @@
-export default function ErrorPage() {
-    return <p>There has been an error!</p>
+
+
+export default async function ErrorPage({searchParams}) {
+    const sp = await searchParams;
+    return <p>There has been an error! <br></br> more information {sp.error}</p>
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,15 @@
+import { JSX } from 'react'
 import {login, signUp} from './actions'
-export default function LoginPage() {
+
+function HandleError({error} : {error: string | null}){
+    if(error === null || error === undefined || error === '') return <></>
+    const cleanError = error.replace('AuthApiError:', '')
+    return <p>{cleanError}</p>
+}
+
+export default  async function LoginPage({searchParams} : 
+    {searchParams : Promise<{error: string | null}>}) {
+    const sp = await searchParams
     return (
         <main className="flex flex-col items-center m-3">
         <form className='flex flex-col gap-3 w-72 justify-center'>
@@ -10,6 +20,7 @@ export default function LoginPage() {
             <button formAction={login}>Login</button>
             <button formAction={signUp}>Sign Up</button>
         </form>
+        <HandleError error={sp.error}/>
         </main>
     )
 }
